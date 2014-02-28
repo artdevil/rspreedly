9a7d3c1111979fc4f052b9366944ed40a26c3462
adhiguna sabril <adhiguna.sabril@gmail.com>
2014-02-28 13:35:27 +0700

    update gem

--- lib/rspreedly/payment_method.rb
+++ lib/rspreedly/payment_method.rb
@@ -4,7 +4,34 @@ module RSpreedly
       attr_accessor :number, :verification_value, :month, 
                     :year, :first_name, :last_name, :card_type,
                     :address1, :address2, :city, :state, :zip, :country,
-                    :phone_number
+                    :phone_number, :customer_id
+      class << self
+        def find(id)
+          return all if id == :all
+
+          begin
+            data = api_request(:get, "/subscribers/#{id}.xml")
+            RSpreedly::PaymentMethod::CreditCard.new(:customer_id => data["subscriber"]['customer_id'])
+          rescue RSpreedly::Error::NotFound
+            nil
+          end
+        end
+      end
+      
+      # update an credit card (more)
+      def update
+        begin
+          update!
+        rescue RSpreedly::Error::Base
+          nil
+        end
+      end
+
+      def update!
+        result = api_request(:put, "/subscribers/#{self.customer_id}.xml", :body => self.to_xml(:outer => 'subscriber', :tag => 'payment-method', :inner => "credit-card"))
+        self.attributes = result["subscriber"]
+        true      
+      end
     end
     class OnFile < Base
        def to_xml(opts=nil)
